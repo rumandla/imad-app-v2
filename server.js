0e23e66342649106f72a23e31ed9614035f534bc
Abhishek Rumandla <rumandla.abhishek@yahoo.com>
2017-02-15 20:46:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/home', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'home.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
